10b950e28bf5a78767010f6b908c0ec33c053358
Swusan <bmoregarfield@gmail.com>
2025-04-05 15:47:38 -0400

    pdf translator update

--- flask-server/pdfToText.py
+++ flask-server/pdfToText.py
@@ -1,10 +1,10 @@
 import pymupdf as fitz
 
-def pdf_to_text(pdf_path, txt_path):
+def pdf_to_text(pdf_path):
     pdf_document = fitz.open(pdf_path)
-    with open(txt_path, "w", encoding="utf-8") as text_file:
-        for page_number in range(len(pdf_document)):
-            page = pdf_document.load_page(page_number)
-            text = page.get_text()
-            text_file.write(text)
+    for page_number in range(len(pdf_document)):
+        page = pdf_document.load_page(page_number)
+        text = page.get_text()
         pdf_document.close()
+    
+    return text
