5a8677aae5c406abf8904a8a95ffaac5142b6b12
Swusan <bmoregarfield@gmail.com>
2025-04-05 15:13:02 -0400

    pdf to text translation script

---
+++ flask-server/pdfToText.py
@@ -0,0 +1,10 @@
+import pymupdf as fitz
+
+def pdf_to_text(pdf_path, txt_path):
+    pdf_document = fitz.open(pdf_path)
+    with open(txt_path, "w", encoding="utf-8") as text_file:
+        for page_number in range(len(pdf_document)):
+            page = pdf_document.load_page(page_number)
+            text = page.get_text()
+            text_file.write(text)
+        pdf_document.close()
